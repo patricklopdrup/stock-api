559255aa555c3f39b36066bab1063d7d99f4a36d
Patrick <patrick.lopdrup@gmail.com>
2022-01-14 20:26:56 +0100

    Refactored user endpoints and started on binance api

--- stock_api/Features/UserFeature/UserModule.cs
+++ stock_api/Features/UserFeature/UserModule.cs
@@ -6,38 +6,9 @@ namespace stock_api.Features.UserFeature
     {
         public IEndpointRouteBuilder MapEndpoint(IEndpointRouteBuilder endpoints)
         {
-            endpoints.MapGet("/user", async (CustomDbContext db) =>
-            {
-                return await db.Users.ToListAsync();
-            });
-
-            endpoints.MapPost("/user", async (UserDto userDto, CustomDbContext db) =>
-            {
-                var user = new User()
-                {
-                    FirstName = userDto.FirstName,
-                    LastName = userDto.LastName,
-                    UserName = userDto.UserName,
-                    Email = userDto.Email
-                };
-
-                db.Users.Add(user);
-                await db.SaveChangesAsync();
-
-                return user;
-            });
-
-            endpoints.MapDelete("/user/{id}", async (int id, CustomDbContext db) =>
-            {
-                if (await db.Users.FindAsync(id) is User user)
-                {
-                    db.Users.Remove(user);
-                    await db.SaveChangesAsync();
-                    return Results.Ok(user);
-                }
-
-                return Results.NotFound();
-            });
+            endpoints.MapGet("/user", GetUsers);
+            endpoints.MapPost("/user", AddUser);
+            endpoints.MapDelete("/user/{id}", DeleteUserById);
 
             return endpoints;
         }
@@ -46,5 +17,43 @@ namespace stock_api.Features.UserFeature
         {
             return builder;
         }
+
+
+        #region CRUD methods
+        internal async Task<List<User>> GetUsers(CustomDbContext db)
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        internal async Task<IResult> AddUser(UserDto userDto, CustomDbContext db)
+        {
+            var user = new User()
+            {
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                UserName = userDto.UserName,
+                Email = userDto.Email
+            };
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+
+            return Results.Ok(user);
+        }
+
+        internal async Task<IResult> DeleteUserById(int id, CustomDbContext db)
+        {
+            if (await db.Users.FindAsync(id) is User user)
+            {
+                db.Users.Remove(user);
+                await db.SaveChangesAsync();
+                return Results.Ok(user);
+            }
+
+            return Results.NotFound();
+        }
+
+
+        #endregion
     }
 }
